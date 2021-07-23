cba8ab577535bc5c8f516f0163eacb51a2ebc05c
Harlin0515 <575821146@qq.com>
2021-07-23 11:24:24 +0800

    Initial commit

---
+++ src/main/java/hello1.java
@@ -0,0 +1,9 @@
+/**
+ * @author Harlin
+ * @create 2021-07-23 11:21
+ */
+public class hello1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/hello2.java
@@ -0,0 +1,9 @@
+/**
+ * @author Harlin
+ * @create 2021-07-23 11:22
+ */
+public class hello2 {
+    public static void main(String[] args) {
+        System.out.println("hello2");
+    }
+}
